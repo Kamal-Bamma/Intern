2afe7bdca3de7ea42e311a2d0982a9dd1faf16a8
Kamal-Bamma <Kamal.Bamma05@gmail.com>
2024-07-10 16:08:37 +0545

    Dis/Del/Edit

--- NodeProjectOne/main.js
+++ NodeProjectOne/main.js
@@ -49,4 +49,53 @@ app.post("/signuppage", async (req, res) => {
   }
 });
 
+app.get("/users", async (req, res) => {
+  try {
+    const users = await collection.find({});
+    res.json(users);
+  } catch (e) {
+    res.status(500).json("Error occurred while fetching users");
+  }
+});
+
+app.delete("/users/:_id", async (req, res) => {
+  const { _id } = req.params;
+
+  try {
+    const result = await collection.deleteOne({ _id });
+
+    if (result.deletedCount > 0) {
+      res.json("User deleted");
+    } else {
+      res.status(404).json("User not found");
+    }
+  } catch (e) {
+    console.error("Error in DELETE /users/:id:", e);
+    res.status(500).json("Error occurred while deleting user");
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { email, password } = req.body;
+  // console.log({ id });
+  try {
+    // const findUser = await collection.findOne({ _id: id });
+    // console.log({ findUser });
+    const result = await collection.updateOne(
+      { _id: id },
+      { $set: { email: email, password: password } }
+    );
+
+    if (result.modifiedCount > 0) {
+      res.json("User updated");
+    } else {
+      res.status(404).json("User not found or no changes made");
+    }
+  } catch {
+    console.error("Error in PUT /users/:id", e);
+    res.status(500).json("Error occurred while updating user");
+  }
+});
+
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
