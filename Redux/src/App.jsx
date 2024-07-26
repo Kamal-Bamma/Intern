f003f56a13ffe6c6b07e0aeb93bd57492e253b53
Kamal-Bamma <Kamal.Bamma05@gmail.com>
2024-07-26 10:31:52 +0545

    redux-counter

--- Redux/src/App.jsx
+++ Redux/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useAppDispatch, useAppSelector } from "./redux/hooks";
 import { increment, decrement } from "./redux/slices/counter";
 import MyComp from "./MyCount";
+// import { logo } from "./assets/react.svg";
 
 function App() {
   const count = useAppSelector((state) => state.counter);
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>Count is {count}</h1>
         <button onClick={() => dispatch(increment())}>Increment</button>
         <button onClick={() => dispatch(decrement())}>Decrement</button>
--- Redux/src/redux/slices/counter/index.jsx
+++ Redux/src/redux/slices/counter/index.jsx
@@ -4,8 +4,8 @@ export const counterSlice = createSlice({
   initialState: 0,
   name: "counter",
   reducers: {
-    increment: (state) => state + 1,
-    decrement: (state) => state - 1,
+    increment: (state) => (state += 1),
+    decrement: (state) => (state -= 1),
   },
 });
 
