4aeec831055e62cf5ee42caa419d33e928843f43
Kamal-Bamma <Kamal.Bamma05@gmail.com>
2024-08-02 15:30:06 +0545

    conversation updated

--- Realtime-chat/routes/messages.js
+++ Realtime-chat/routes/messages.js
@@ -10,14 +10,14 @@ router.get("/index", authenticate, async (req, res) => {
   try {
     const userId = req.user ? req.user._id : null;
     const users = await User.find({ _id: { $ne: userId } });
-    const messages = await Message.find({
-      $or: [{ sender: userId }, { receiver: userId }],
-    })
-      .populate("sender", "name")
-      .populate("receiver", "name")
-      .sort("timestamp");
+    // const messages = await Message.find({
+    //   $or: [{ sender: userId }, { receiver: userId }],
+    // })
+    //   .populate("sender", "name")
+    //   .populate("receiver", "name")
+    //   .sort("timestamp");
 
-    res.render("index.ejs", { messages, users, user: req.user });
+    res.render("index.ejs", { users, user: req.user });
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -53,17 +53,24 @@ router.get("/index", authenticate, async (req, res) => {
 router.post("/index", async (req, res) => {
   try {
     const { senderId, receiverId, content } = req.body;
-    console.log(req.body);
     if (!senderId || !receiverId || !content) {
       return res.status(400).send({ message: "Missing info" });
     }
+    const conversation = await Message.findOne({ senderId, receiverId });
 
-    const messages = new Message({
-      senderId,
-      receiverId,
-      content: [{ text: content }],
-    });
-    await messages.save();
+    if (!conversation) {
+      const messages = new Message({
+        senderId,
+        receiverId,
+        content: [{ text: content }],
+      });
+      await messages.save();
+    } else {
+      conversation.content.push({ text: content });
+      await conversation.save();
+    }
+    console.log(conversation);
+
     res.status(200).send("Message send");
   } catch (e) {
     res.status(500).send({ message: e.message });
